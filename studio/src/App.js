7f8f13b6f1bae75fae1d1260c34d0c208a126310
iamzachb <pimpofthecentury1@yahoo.com>
2023-08-03 19:58:37 -0500

    Finished recipe description

---
+++ studio/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import RecipeDescription from './components/Description';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <RecipeDescription />
+      </div>
+    </div>
+  );
+}
+
+export default App;
