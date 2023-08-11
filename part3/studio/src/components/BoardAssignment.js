56e97ad47de8b9aa518155e15d4d3ab43b063aef
iamzachb <pimpofthecentury1@yahoo.com>
2023-08-10 20:14:38 -0500

    Completed studio

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  const boards = [
+    {
+      label: "Fun with Friends",
+      value: "Fun with Friends",
+    },
+    {
+      label: "Fine Dining",
+      value: "Fine Dining",
+    },
+    {
+      label: "Sweeties",
+      value: "Sweeties",
+    },
+  ];
+  const [boardName, setName] = useState("no boards yet!");
 
-   const handleChange = (event) => {
-   }
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        {boards.map((item) => (
+          <option value={item.value}>{item.label}</option>
+        ))}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,63 +1,75 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+  let authorLink = "https://en.wikipedia.org/wiki/File:Tuna_Casserole2.jpg";
+  let authorPhoto =
+    "https://en.wikipedia.org/wiki/File:Chef_Tom_Wandera_at_work.jpg";
+  let authorName = "JAICARD";
 
-   return (
+  return (
+    <div>
+      <img
+        src={authorPhoto}
+        alt=""
+        style={{ objectFit: "contain", borderRadius: "50%" }}
+      />
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
-         <div>
-            <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
-         </div>
+        <h3>{authorName}</h3>
+        <a href={authorLink}></a>
       </div>
-   );
-}
+    </div>
+  );
+};
 
 const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
-      <div>
-         <h3>Recipe Ingredients</h3>
-         <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
-         </ul>
-      </div>
-   );
-}
+  const ingredients = [
+    "1 (12 ounce) package egg noodles",
+    "2 (10.5 ounce) cans condensed cream of mushroom soup",
+    "2 cups shredded Cheddar cheese, divided",
+    "2 (5 ounce) cans tuna, drained",
+    "1 cup frozen green peas",
+  ];
+  return (
+    <div>
+      <h3>Recipe Ingredients</h3>
+      <ul>
+        <li>{ingredients[0]}</li>
+        <li>{ingredients[1]}</li>
+        <li>{ingredients[2]}</li>
+        <li>{ingredients[3]}</li>
+        <li>{ingredients[4]}</li>
+      </ul>
+    </div>
+  );
+};
 
 const RecipeDescription = () => {
-   return (
-      <div> 
-         <div>
-            <h1></h1>
-            <p></p>
-         </div>
-         <div className="recipePhotoBlock">
-            <RecipeIngredients />
-            <RecipeAuthor />
-         </div>
+  return (
+    <div>
+      <div>
+        <h1>Tune Casserole</h1>
+        <p>
+          This is a tuna casserole that even my picky family loves! The potato
+          chips give the casserole a crunchy crust.
+        </p>
       </div>
-   );
-}
+      <div className="recipePhotoBlock">
+        <RecipeIngredients />
+        <RecipeAuthor />
+      </div>
+    </div>
+  );
+};
 
 const RecipePhoto = () => {
-   return (
-      <img src="" alt="" className="imageUpdates"/>
-   );
-}
+  return <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Tuna_Casserole2.jpg" alt="Tuna Casserole" className="imageUpdates" />;
+};
 
-export default function RecipeDisplay () {
-   return(
-      <div className="recipePhotoBlock">
-         <RecipePhoto />
-         <div>
-            <RecipeDescription />
-         </div>
+export default function RecipeDisplay() {
+  return (
+    <div className="recipePhotoBlock">
+      <RecipePhoto />
+      <div>
+        <RecipeDescription />
       </div>
-   );
+    </div>
+  );
 }
